d9c7a1d587733d52e6e4bbf14c8128c8ec480527
lucifer-091 <work.ajay713347@gmail.com>
2025-03-27 02:55:22 +0530

    env chnages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
   appVersion: packageInfo.version,
   production: false,
   imgUrl: 'http://45.79.124.136:9000/',
-  // apiUrl: 'http://45.79.124.136:9000/v1/',
-  // backendUrl : 'http://45.79.124.136:9000/v1/',
-  apiUrl: 'http://localhost:9000/v1/',
-  backendUrl : 'http://localhost:9000/v1/',
+  apiUrl: 'http://45.79.124.136:9000/v1/',
+  backendUrl : 'http://45.79.124.136:9000/v1/',
+  // apiUrl: 'http://localhost:9000/v1/',
+  // backendUrl : 'http://localhost:9000/v1/',
 };
 
 /*
