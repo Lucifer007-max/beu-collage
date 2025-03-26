055bbda07c814b3e6a433609ef8e35fa7de188c9
lucifer-091 <work.ajay713347@gmail.com>
2025-03-27 02:59:15 +0530

    env chnages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://paramapi.getmocktest.com/api/'
+  imgUrl: 'http://45.79.124.136:9000/',
+  apiUrl: 'http://45.79.124.136:9000/v1/',
+  backendUrl : 'http://45.79.124.136:9000/v1/',
 };
