f00e5af267ea71b4630e1c091851a1c47c359265
lucifer-091 <work.ajay713347@gmail.com>
2025-04-26 13:47:39 +0530

    server chnage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  imgUrl: 'http://localhost:9001/',
-  // apiUrl: 'https://beu.anantdrishti.com/v1/',
-  // backendUrl : 'https://beu.anantdrishti.com/v1/',
-  apiUrl: 'http://localhost:9001/v1/',
-  backendUrl : 'http://localhost:9001/v1/',
+  imgUrl: 'https://beu.anantdrishti.com/',
+  apiUrl: 'https://beu.anantdrishti.com/v1/',
+  backendUrl : 'https://beu.anantdrishti.com/v1/',
+  // apiUrl: 'http://localhost:9001/v1/',
+  // backendUrl : 'http://localhost:9001/v1/',
 };
 
 /*
