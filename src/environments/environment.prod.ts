259bbfddab6df05a31748dd41092d92b7f571859
Ajay <81299123+Lucifer007-max@users.noreply.github.com>
2025-06-23 11:30:11 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,14 @@ export const environment = {
   imgUrl: 'https://beu-bih.ac.in/backend/',
   apiUrl: 'https://beu-bih.ac.in/backend/v1/',
   backendUrl : 'https://beu-bih.ac.in/backend/v1/',
+  firebaseConfig : {
+    apiKey: "AIzaSyASbS80xS1_w2UWb_UlBxngzeAGDaIbCsc",
+    authDomain: "beu-app-10946.firebaseapp.com",
+    projectId: "beu-app-10946",
+    storageBucket: "beu-app-10946.firebasestorage.app",
+    messagingSenderId: "49555334698",
+    appId: "1:49555334698:web:1eadefbc40cfa6fe898553",
+    measurementId: "G-BZZ81KMPXJ"
+  }
+
 };
